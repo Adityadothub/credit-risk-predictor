29a8591cc6ab82be905a109bab88c224c4404b13
Adityadothub <anandaditya2125@gmail.com>
2026-07-19 14:21:37 +0530

    Configure MongoDB connection for Streamlit Cloud

--- database/connection.py
+++ database/connection.py
@@ -1,11 +1,17 @@
 from pymongo import MongoClient
 from dotenv import load_dotenv
+import streamlit as st
 import os
 
 load_dotenv()
 
-client = MongoClient(os.getenv("CR_DB"))
+# Use .env locally, Streamlit Secrets in deployment
+mongo_uri = os.getenv("CR_DB")
+
+if not mongo_uri:
+    mongo_uri = st.secrets["CR_DB"]
+
+client = MongoClient(mongo_uri)
 
 db = client["credit_risk_db"]
-
 predictions_collection = db["predictions"]
